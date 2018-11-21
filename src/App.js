39495ff3dae188d609a3a71cb3f67b391c8e64fe
aharxict <it.ahapx@gmail.com>
2018-11-21 18:21:43 +0200

    added ability to get data from server and show one post

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Blog />
       </div>
     );
   }
