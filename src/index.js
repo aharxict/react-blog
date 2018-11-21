644848f566b07b1cc9ea8e7fc56884a679dc7d31
aharxict <it.ahapx@gmail.com>
2018-11-21 19:27:37 +0200

    interceptors implemented

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+  console.log('interceptors request', request);
+  return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  console.log('interceptors response', response);
+  return response;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
